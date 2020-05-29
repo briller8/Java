25d87485323db528a62d9ddff6b1f01c9ff6ec81
briller8 <briller015@gmail.com>
2020-05-29 23:08:46 +0900

    add

---
+++ basic/Object/Object/별찍기.java
@@ -0,0 +1,13 @@
+package Object.Object;
+
+public class 별찍기 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<3;i++) {
+			for(int j=0; j<5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
